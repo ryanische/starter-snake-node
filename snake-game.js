92f52a6a5c7e2258a568e809e1a6465a086f9356
Ryan Ische <ryan.ische@vidyard.com>
2019-11-28 23:33:08 -0500

    scoring?

--- snake-game.js
+++ snake-game.js
@@ -68,37 +68,48 @@ class SnakeGame {
     const me = gameState.you;
     const currentLoc = me.body[0];
 
-    let moves = this.filterForwardOnly(this.prevMove, MOVES);
-    moves = this.filterWalls(currentLoc, moves);
-    moves = this.filterSnakes(currentLoc, gameState.board.snakes, moves);
-
-    console.log('MOVES', moves);
-
-    const move = moves[Math.floor(Math.random() * moves.length)];
-
-    this.prevLoc = currentLoc;
-    this.prevMove = move;
-
-    console.log(this.prevLoc, this.prevMove, me.health);
-
-    return move;
-  }
-
-  filterSnakes(currentLoc, snakes, moves) {
-    const snakeLocs = [];
+    const grid = [];
+    const snakes = gameState.board.snakes;
+    const food = gameState.board.food;
 
     for (let x = 0; x < this.board.width; x++) {
-      snakeLocs[x] = [];
+      grid[x] = [];
       for (let y = 0; y < this.board.height; y++) {
-        snakeLocs[x][y] = 0;
+        grid[x][y] = 0;
       }
     }
 
     _.flatMap(snakes, s => s.body).forEach(body => {
-      snakeLocs[body.y][body.x] = 1;
+      grid[body.y][body.x] = 1;
     });
-    console.log(snakeLocs);
 
+    let moves = this.filterForwardOnly(this.prevMove, MOVES);
+    moves = this.filterWalls(currentLoc, moves);
+    moves = this.filterSnakes(currentLoc, grid, moves);
+
+    food.forEach(f => {
+      grid[f.y][f.x] = 2;
+    });
+
+    console.log(grid);
+
+    const scores = _.sortBy(this.scoreMoves(moves, currentLoc, grid), ['score']);
+    console.log(scores);
+    console.log('MOVES', moves);
+
+    // const move = moves[Math.floor(Math.random() * moves.length)];
+
+    const move = this.randomByScore(scores);
+
+    this.prevLoc = currentLoc;
+    this.prevMove = move;
+
+    console.log(currentLoc, move, me.health, gameState.turn);
+
+    return move;
+  }
+
+  filterSnakes(currentLoc, snakeLocs, moves) {
     return _.filter(moves, m => {
       const potentialLoc = newLocs[m](currentLoc);
       return snakeLocs[potentialLoc.y][potentialLoc.x] === 0;
@@ -127,8 +138,64 @@ class SnakeGame {
     return _.difference(moves, crashMoves);
   }
 
-  scoreMove(move, currentLoc, grid) {
-    
+  scoreMoves(moves, currentLoc, grid) {
+    return moves.map(m => {
+      let gridPredicate;
+      if (m === 'up') {
+        gridPredicate = (x, y) => {
+          return y < currentLoc.y && x < (currentLoc.x + 4) && x > (currentLoc.x - 4);
+        };
+      } else if (m === 'down') {
+        gridPredicate = (x, y) => {
+          return y > currentLoc.y && x < (currentLoc.x + 4) && x > (currentLoc.x - 4);
+        };
+      } else if (m == 'right') {
+        gridPredicate = (x, y) => {
+          return x > currentLoc.x && y < (currentLoc.y + 4) && y > (currentLoc.y - 4);
+        };
+      } else { // left
+        gridPredicate = (x, y) => {
+          return x < currentLoc.x && y < (currentLoc.y + 4) && y > (currentLoc.y - 4);
+        };
+      }
+
+      let score = 0;
+
+      for (let y = 0; y < grid.length; y++) {
+        let row = grid[y];
+        for (let x = 0; x < row.length; x++) {
+          if (!gridPredicate(x, y)) {
+            continue;
+          }
+
+          if (grid[y][x] === 0) {
+            score++;
+          }
+
+          if (grid[y][x] === 2) {
+            score = score + 100;
+          }
+        }
+      }
+
+      return { move: m, score: score };
+    });
+  }
+
+  randomByScore(scores) {
+    const totalScore = _.sum(scores.map(s => s.score));
+    const rand = Math.random();
+    let sum = 0;
+
+    scores.forEach(s => {
+      const chance = s.score / totalScore;
+      sum += chance;
+      if (rand <= sum) {
+        return s.move;
+      }
+    });
+
+    return scores[scores.length - 1].move;
   }
 }
 
